dd613615459419b8e90e8cf77bb7826df1b9f1a3
“CCRNP” <1194433107@qq.com>
2021-10-10 15:33:51 +0800

    change format to Babel

--- docs/.vuepress/config/pluginConf.js
+++ docs/.vuepress/config/pluginConf.js
@@ -1,41 +1,50 @@
-const secret = require("./secret")
+const secret = require("./secret.js")
 
 module.exports = {
+  // vssue 评论功能
+  plugins:[
+    [
+      '@vssue/vuepress-plugin-vssue', {
+        // 设置 `platform` 而不是 `api`
+        platform: 'github-v4',
+        // 其他的 Vssue 配置
+        owner: 'CCRNP',
+        repo: 'VuePress-Starter',
+        clientId: secret.clientId,
+        clientSecret: secret.clientSecret,
+      }
+    ],
+    // last-updated
+    [
+      '@vuepress/last-updated', {
+        transformer: (timestamp, lang) => {
+          // 不要忘了安装 moment
+          const moment = require('moment')
+          moment.locale("zh-cn")
+          return moment(timestamp).format("LLLL")
+        }
+      }
+    ]
+    // PWA
+    [
+      '@vuepress/pwa', {
+        serviceWorker: true,
+        updatePopup: {
+          message: "New content is available.",
+          buttonText: "Refresh"
+        }
+      }
+    ],
 
-  '@vssue/vuepress-plugin-vssue': {
-    // 设置 `platform` 而不是 `api`
-    platform: 'github-v4',
-    // 其他的 Vssue 配置
-    owner: 'CCRNP',
-    repo: 'VuePress-Starter',
-    clientId: 'serect.clientID',
-    clientSecret: 'serect.clientSecret',
-  },
+    // Back to Top
+    ['@vuepress/back-to-top', true],
 
-  '@vuepress/last-updated': {
-    transformer: (timestamp, lang) => {
-      // 不要忘了安装 moment
-      const moment = require('moment')
-      moment.locale("zh-cn")
-      return moment(timestamp).format("LLLL")
-    }
-  },
-
-  '@vuepress/pwa': {
-    serviceWorker: true,
-    updatePopup: {
-      message: "New content is available.",
-      buttonText: "Refresh"
-    }
-  },
-
-  // Back to Top
-  '@vuepress/back-to-top': true,
-
-  //Google Analytics
-  '@vuepress/google-analytics':
-  {
-    'ga': secret.ga
-  }
+    //Google Analytics
+    [
+      '@vuepress/google-analytics', {
+        'ga': secret.ga
+      }
+    ]
+  ]
 
 }
